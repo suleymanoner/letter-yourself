d58216d7857f574e43421df1ae812ac5cd7c88e8
suleyman <suleymanoner1999@gmail.com>
2021-03-12 21:31:30 +0100

    person to send dao added

---
+++ api/dao/PersonToSentDao.class.php
@@ -0,0 +1,12 @@
+<?php
+require_once dirname(__FILE__)."/BaseDao.class.php";
+
+class PersonToSentDao extends BaseDao{
+
+  public function __construct(){
+    parent::__construct("person_to_sent");
+  }
+
+}
+
+?>
