cd8a55cbe16d0eca684b4e5166ab13f30af7fb79
suleyman <suleymanoner1999@gmail.com>
2021-03-14 22:53:44 +0100

    extra dao classes added

---
+++ api/dao/PhotographContentDao.class.php
@@ -0,0 +1,14 @@
+<?php
+require_once dirname("__FILE__")."/BaseDao.class.php";
+
+class PhotographContentDao extends BaseDao{
+
+  public function __construct(){
+    parent::__construct("photograph_content");
+  }
+
+
+}
+
+
+?>
---
+++ api/dao/TextContentDao.class.php
@@ -0,0 +1,13 @@
+<?php
+require_once dirname(__FILE__)."/BaseDao.class.php";
+
+class TextContentDao extends BaseDao{
+
+  public function __construct(){
+    parent::__construct("text_content");
+  }
+
+}
+
+
+?>
