0c241d932e6c7ddccd1376c6e5e3a84ce6045f1b
suleyman <suleymanoner1999@gmail.com>
2021-03-16 00:06:20 +0100

    flight installed and some routes added

--- api/index.php
+++ api/index.php
@@ -1,17 +1,37 @@
 <?php
 
-/*
--- Displaying errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-*/
 
 
-require_once dirname(__FILE__)."/dao/BaseDao.class.php";
+require dirname(__FILE__).'/../vendor/autoload.php';
+require dirname(__FILE__).'/dao/AccountDao.class.php';
 
 
-$suyo = new BaseDao();
+Flight::register('accountDao','AccountDao');
 
-echo "Hello from API";
+Flight::route('GET /accounts', function(){
+    Flight::json(Flight::accountDao()->get_all(0,2));
+});
+
+Flight::route('GET /accounts/@id', function($id){
+    Flight::json(Flight::accountDao()->get_by_id($id));
+});
+
+Flight::route('POST /accounts', function(){
+    $request = Flight::request()->data->getData();
+    Flight::json(Flight::accountDao()->add($data));
+});
+
+Flight::route('PUT /accounts/@id', function($id){
+    $request = Flight::request();
+    $data = $request->data->getData();
+    Flight::accountDao()->update($id, $data);
+    $account = Flight::accountDao()->get_by_id($id);
+    Flight::json($account);
+});
+
+Flight::start();
+
 ?>
