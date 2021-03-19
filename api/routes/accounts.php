3fc2e595adc28df3914b697c23ed90c5a782ca54
suleyman <suleymanoner1999@gmail.com>
2021-03-19 22:06:04 +0100

    search option for account added

--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -3,7 +3,13 @@
 Flight::route('GET /accounts', function(){
   $offset = Flight::query('offset', 0);
   $limit = Flight::query('limit', 10);
-  Flight::json(Flight::accountDao()->get_all($offset, $limit));
+  $search = Flight::query('search');
+  if($search){
+    Flight::json(Flight::accountDao()->get_accounts($search, $offset, $limit));
+  }
+  else{
+    Flight::json(Flight::accountDao()->get_all($offset, $limit));
+  }
 });
 
 Flight::route('GET /accounts/@id', function($id){
