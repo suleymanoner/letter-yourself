e56851e755e878693446f5074e88a9179cd0cc4f
suleyman <suleymanoner1999@gmail.com>
2021-04-11 12:55:24 +0200

    added origin url

--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -5,7 +5,7 @@
  * @OA\OpenApi(
  *   @OA\Server(url="http://localhost/letteryourself/api/", description="Development Environment"),
  *   @OA\Server(url="https://ly.letteryourself.live/api/", description="Production Environment"),
- *   @OA\Server(url="https://letter-yourself-me-djt6q.ondigitalocean.app/api", description="Production Environment2")
+ *   @OA\Server(url="https://letter-yourself-oed23.ondigitalocean.app/api/", description="Production Environment2")
  * ),
  * @OA\SecurityScheme(
  *      securityScheme="ApiKeyAuth",
